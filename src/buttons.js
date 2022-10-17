aed763a937109eb7f70ea30cfe279c369c7b582b
Sara <sara.spreitzhofer@stud.fh-campuswien.ac.at>
2022-10-17 15:52:29 +0200

    on click (not working)

--- src/buttons.js
+++ src/buttons.js
@@ -1,6 +1,6 @@
-    class Buttons {
+    /*class Buttons {
 
-        /*feedbackText(){
+        feedbackText(){
             let text = document.createElement("p");
             text.innerHTML = "Thanks for feeding me :)"
             let scene = document.getElementById("scene");
@@ -12,17 +12,23 @@
             let feed = document.getElementById("feed");
             feed.addEventListener("click", this.feedbackText)
             console.log(feed);
-        }*/
+        }
 
         //let button = document.getElementById("feed").addEventListener("onC")
-    }
+    }*/
 
     function feedbackText(){
-        let text = document.createElement("p");
-        text.innerHTML = "Thanks for feeding me :)"
-        let scene = document.getElementById("scene");
-        scene.appendChild(text);
-        console.log("Yay food")
+        console.log("hier")
+        let text = document.getElementById("paragraph");
+        text.innerHTML = "Thanks for feeding me :)";
+        //document.getElementById("paragraph").innerHTML = "Thanks for feeding me :)";
+        //text.innerText = "Thanks for feeding me :)"
+        let scene = document.querySelector('a-scene');
+        console.log(scene)
+        /*if (scene.hasLoaded) {
+            scene.appendChild(text);
+            console.log("Yay food")
+        }*/
 
     }
 
@@ -32,5 +38,6 @@
         console.log(feed);
     }
 
+    build()
     //const buttons = new Buttons();
     //buttons.build()
