49a5d11225a16e751d0e99dc64287ffddca0baf6
Sara <sara.spreitzhofer@stud.fh-campuswien.ac.at>
2022-11-07 17:06:14 +0100

    onsumbit (doesn't work yet)

--- src/profile.js
+++ src/profile.js
@@ -1,16 +1,27 @@
 
 let profileDiv = document.getElementById("profile")
 // let name = document.getElementById("animal_name")
-let name = "test"
+// let name = "test"
 
-let text = document.createElement("h1")
-let helloText = "Hello " + name + "!"
-text.innerText = helloText
+//let text = document.createElement("h1")
+//let helloText = "Hello " + name + "!"
+//text.innerText = helloText
 
 let profilePic = document.createElement("img")
 profilePic.src = "./assets/2d/dog.png"
 profilePic.id = "profile_pic"
 profilePic.alt = "dog_profile"
 
-profileDiv.appendChild(text)
-profileDiv.appendChild(profilePic)
+//profileDiv.appendChild(text)
+//profileDiv.appendChild(profilePic)
+
+
+function submitForm(e){
+    //e.preventDefault()
+    let name2 = document.getElementById("animal_name")
+    console.log(name2)
+    //let name = document.getElementById("animal_name").value
+    //console.log(name)
+    //alert(name)
+
+}
