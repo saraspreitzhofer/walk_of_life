30487dd864600d47168fcfa1aba4ee25bbf3fcda
Johannes Kasser <johannes.kasser@stud.fh-campuswien.ac.at>
2022-11-30 23:47:53 +0100

    cleanup code

--- src/animations.js
+++
@@ -1,7 +0,0 @@
-var dog = document.getElementById("avatarModel");
-
-dog.setAttribute('food',
-    {'property': position,
-           'to': {x: 0, y: 1, z: 0},
-           'startEvents': 'startFoodAnimation'}
-)
